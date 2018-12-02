90b9ef3bae3b3ca4f72c9bfdfc46ff30d5f21b5f
Alexandra Sazhin <alexandra@sazhin.us>
2018-12-02 16:02:29 -0500

    events angular

--- js/personell.js
+++ js/personell.js
@@ -1,7 +1,20 @@
 var personellApp = angular.module('personellPageApp', []);
 
 personellApp.controller('officerController', ['$scope', function($scope){
-    $scope.officers = [{"Name":"David Lepore","Class":"Junior","Position":"President","Image":"images/david_lepore.jpg"},{"Name":"David Lepore","Class":"Junior","Position":"President","Image":"images/david_lepore.jpg"}];
+    $scope.officers = [
+        {
+            "Name":"David Lepore",
+            "Class":"Junior",
+            "Position":"President",
+            "Image":"images/david_lepore.jpg"
+        },
+        {
+            "Name":"David Lepore",
+            "Class":"Junior",
+            "Position":"President",
+            "Image":"images/david_lepore.jpg"
+        }
+    ];
     
     
     
