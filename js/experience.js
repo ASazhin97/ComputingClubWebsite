bd0d3b7676f2e07adad89717781b4598b7238e72
validName <JJShevchenko@quinnipiac.edu>
2018-12-03 19:20:13 -0500

    donzo

--- js/experience.js
+++ js/experience.js
@@ -45,4 +45,9 @@ newsApp.controller("newsController", ['$scope', function($scope) {
     return false;
   }
 
+  $scope.removeArticle = function(article) {
+  var index = $scope.articles.indexOf(article);
+  $scope.articles.splice(index, 1);
+}
+
 }]);
