a87688b73f6213b3fae5bf9b23a1626496b2fc1c
Daron McIntosh <mcintoshdaron@gmail.com>
2019-04-04 19:20:10 -0400

    add delete, close connection, update connectionURL

--- tests/testUser.js
+++ tests/testUser.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/user');
 
-let connectionURL = 'mongodb+srv://iSolution:VKGODmMbst6tfXe9@thecluster-muogm.mongodb.net/test?retryWrites=true';
+let connectionURL = 'mongodb+srv://SER320User:etr21HbP7gaHLWkx@thecluster-muogm.mongodb.net/ser320db?retryWrites=true';
 mongoose.connect(connectionURL, {
 	useNewUrlParser: true
 });
@@ -16,6 +16,7 @@ dbConnection.on('error', (err) => {
 });
 
 dbConnection.once('open', () => {
+	// Create a user using the model
 	let newUser = new User({
 		year: yearEnums[0],
 		firstName: 'John',
@@ -24,9 +25,22 @@ dbConnection.once('open', () => {
 		summary: 'Hi I\'m John the business major',
 		type: typeEnums[0],
 	});
+	// Save the user to the db
 	User.create(newUser, (err, user) => {
-		if (err) return console.err(err);
+		if (err) return console.log(err);
 		console.log('User was saved');
 		console.log(user);
+		// Delete the user
+		User.findOneAndDelete({
+			firstName: 'John'
+		}, (err, user) => {
+			if (err) return console.log(err.message);
+			console.log('User was deleted');
+			console.log(user);
+		});
+		// Close the connection
+		dbConnection.close((err) => {
+			if (err) return console.log(err.message);
+		});
 	});
 });
