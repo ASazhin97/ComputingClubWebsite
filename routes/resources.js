244ade5f7acad4a4eef7abafcad5c1abb4c2451e
Daron McIntosh <mcintoshdaron@gmail.com>
2019-04-14 19:14:36 -0400

    add functions to resources route to save to db

--- routes/resources.js
+++ routes/resources.js
@@ -1,31 +1,73 @@
 const express = require('express');
 const auth = require('./auth');
+const Resource = require('../models/resource');
 const router = express.Router();
 const verifyAdmin = auth.verifyAdmin;
 
-router.get('/', (req, res) => {
-  res.send('<h1>Resources Page</h1>');
+// const categoryEnums = Resource.schema.path('category').enumValues;
+
+router.get('/', (req, res, next) => {
+  Resource.find({}, (err, resources) => {
+    if (err){
+      return next(err);
+    }
+    res.json(resources);
+  });
+});
+
+router.post('/', (req, res, next) => {
+  // Create a resource using the model
+  const newResource = new Resource({
+    title: req.body.title,
+    summary: req.body.summary,
+    link: req.body.link,
+    category: req.body.category,
+  });
+  // Save the resource to the db
+  Resource.create(newResource, (err, resource) => {
+    if (err) return next(err);
+    console.log('Resource was saved');
+    res.json(resource);
+  });
 });
 
 // TODO: Do validation to ensure category is valid. Use enums from model
-router.get('/category/:name', (req, res) => {
-  res.send(`<h1>Resources Page: Category is ${req.params.name}</h1>`);
+router.get('/category/:name', (req, res, next) => {
+  Resource.find({category: req.params.name}, (err, resources) => {
+    if (err){
+      return next(err);
+    }
+    res.json(resources);
+  });
 });
 
 // TODO: Admin only routes. (Should GET be admin only?)
 router.route('/:id')
     .all(verifyAdmin)
-    .get((req, res) => {
-      res.send('<h1>Resources Page: GET ID</h1>');
+    .get((req, res, next) => {
+      Resource.findById(req.params.id, (err, resource) => {
+        if (err){
+          return next(err);
+        }
+        res.json(resource);
+      });
     })
-    .post((req, res) => {
-      res.send('<h1>Resources Page: POST ID</h1>');
+    .put((req, res, next) => {
+      Resource.findByIdAndUpdate(req.params.id, req.body.resource,
+          (err, resource) => {
+            if (err){
+              return next(err);
+            }
+            res.json(resource);
+          });
     })
-    .put((req, res) => {
-      res.send('<h1>Resources Page: PUT ID</h1>');
-    })
-    .delete((req, res) => {
-      res.send('<h1>Resources Page: DELETE ID</h1>');
+    .delete((req, res, next) => {
+      Resource.findByIdAndDelete(req.params.id, (err, resource) => {
+        if (err){
+          return next(err);
+        }
+        res.json(resource);
+      });
     });
 
 module.exports = router;
