c66d62a67a245b7b14211be1d6b1e6eb2c1f8dec
Daron McIntosh <mcintoshdaron@gmail.com>
2019-04-14 19:33:12 -0400

    add comments and a better logger

--- routes/resources.js
+++ routes/resources.js
@@ -6,6 +6,8 @@ const verifyAdmin = auth.verifyAdmin;
 
 // const categoryEnums = Resource.schema.path('category').enumValues;
 
+// GET /resources
+// Returns all resources
 router.get('/', (req, res, next) => {
   Resource.find({}, (err, resources) => {
     if (err){
@@ -15,6 +17,8 @@ router.get('/', (req, res, next) => {
   });
 });
 
+// POST /resources
+// Creates one resource
 router.post('/', (req, res, next) => {
   // Create a resource using the model
   const newResource = new Resource({
@@ -23,15 +27,16 @@ router.post('/', (req, res, next) => {
     link: req.body.link,
     category: req.body.category,
   });
-  // Save the resource to the db
+  // Save the resource
   Resource.create(newResource, (err, resource) => {
     if (err) return next(err);
-    console.log('Resource was saved');
     res.json(resource);
   });
 });
 
 // TODO: Do validation to ensure category is valid. Use enums from model
+// GET /resources/category/:name
+// Returns all resources that are in the category 'name'
 router.get('/category/:name', (req, res, next) => {
   Resource.find({category: req.params.name}, (err, resources) => {
     if (err){
@@ -42,6 +47,8 @@ router.get('/category/:name', (req, res, next) => {
 });
 
 // TODO: Admin only routes. (Should GET be admin only?)
+// GET/PUT/DELETE /resources/:id
+// Returns/Updates/Deletes one resource by the resource's id
 router.route('/:id')
     .all(verifyAdmin)
     .get((req, res, next) => {
