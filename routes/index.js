b2aa318c3de9819d847e1e77b90666d576f692ad
Alexandra Sazhin <alexandra@sazhin.us>
2019-04-15 11:05:41 -0400

    attempt 2 still not working

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,25 @@
 const express = require('express');
 const router = express.Router();
 
-// set up files
+// get files
+const admin = require('./admin');
 const events = require('./events');
+const members = require('./members');
+const personnel = require('./personnel');
+const resources = require('./resources');
+const applications = require('./applications');
 
-// sending to the different routers
+// send to respoective routers
+router.use('/admin', admin);
 router.use('/events', events);
+router.use('/members', members);
+router.use('/personnel', personnel);
+router.use('/resources', resources);
+router.use('/applications', applications);
+
+// get homepage
+router.get('/', (req, res) => {
+  res.send('<h1>Home Page</h1>');
+});
 
 module.exports = router;
