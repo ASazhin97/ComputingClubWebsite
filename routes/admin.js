0b351b718a2c9defa1f312ec28ceab0c1712169c
Daron McIntosh <mcintoshdaron@gmail.com>
2019-04-16 21:12:14 -0400

    add comments to admin route

--- routes/admin.js
+++ routes/admin.js
@@ -2,21 +2,28 @@ const express = require('express');
 const auth = require('./auth');
 const router = express.Router();
 
-
+// GET/POST /admin/register
 router.route('/register')
+    // Sends registration page
     .get((req, res) => {
       res.send('<h1>Admin Register GET</h1>');
     })
+    // Creates a new admin user
     .post((req, res, next) => {
       auth.registerAdmin(req, res, next);
     });
 
+// GET/POST /admin/login
 router.route('/login')
+    // Sends login page
     .get((req, res) => {
       res.send('<h1>Admin Login GET</h1>');
     })
+    // Authenticate admin user
     .post(auth.loginAdmin);
 
+// GET /admin/logout
+// Logout admin user
 router.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/');
