a03f2a57429a66c6c0e21451ecca46e5c3aef87a
Daron McIntosh <mcintoshdaron@gmail.com>
2019-04-19 22:02:37 -0400

    update config to use different db for prod & dev

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
-if (process.env.NODE_ENV !== 'production'){
+const isProduction = process.env.NODE_ENV === 'production';
+const developmentUrl = 'mongodb+srv://SER320User:etr21HbP7gaHLWkx@thecluster-muogm.mongodb.net/ser320devdb?retryWrites=true';
+const productionUrl = 'mongodb+srv://SER320User:etr21HbP7gaHLWkx@thecluster-muogm.mongodb.net/ser320proddb?retryWrites=true';
+
+// Use different databases for production and development
+const mongoURL = isProduction ? productionUrl : developmentUrl;
+
+// GET environment variables from .env file if we are not in production
+if (!isProduction){
   require('dotenv').config();
 }
 
 module.exports = {
   'secretKey': process.env.SESSION_SECRET,
-  'mongoURL': 'mongodb+srv://SER320User:etr21HbP7gaHLWkx@thecluster-muogm.mongodb.net/ser320db?retryWrites=true',
+  'mongoURL': mongoURL,
 };
