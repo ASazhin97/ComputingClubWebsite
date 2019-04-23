0451b8e76cb7e828d2ced509dc443f32a0cd3168
Daron McIntosh <mcintoshdaron@gmail.com>
2019-04-23 10:16:34 -0400

    finish jwt implementation

--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,11 @@ const log = require('loglevel');
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
-
-module.exports.getToken = callback => {
-  jwt.sign({admin: true}, config.secretKey, (err, token) => {
+// Secret code will be passed in the body and that will determine if a user
+// is an admin or not. For now it defaults to true
+// The only thing stored in the payload is an object: {isAdmin:boolean}
+module.exports.getToken = (isAdmin, callback) => {
+  jwt.sign({admin: isAdmin}, config.secretKey, (err, token) => {
     if (err){
       callback(err, null);
     }
@@ -14,6 +16,7 @@ module.exports.getToken = callback => {
     callback(null, token);
   });
 };
+
 module.exports.registerAdmin = (req, res, next) => {
   log.info('Registering user');
   Admin.register(new Admin({
@@ -24,7 +27,8 @@ module.exports.registerAdmin = (req, res, next) => {
       return next(err);
     }
     log.info('User registered');
-    this.getToken((err, token) => {
+
+    this.getToken(true, (err, token) => {
       if (err){
         next(err);
       }
@@ -44,7 +48,7 @@ module.exports.loginAdmin = (req, res, next) => {
         if (!user){
           return res.redirect('/admin/login');
         }
-        this.getToken((err, token) => {
+        this.getToken(true, (err, token) => {
           if (err){
             next(err);
           }
