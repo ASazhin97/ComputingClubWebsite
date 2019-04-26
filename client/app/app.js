d7a3395954f073f56395ac16e0284c4b99e1e91d
Daron McIntosh <mcintoshdaron@gmail.com>
2019-04-25 22:02:09 -0400

    uncomment and add injectFile to /events

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,7 @@ angular
     .config($routeProvider => {
       $routeProvider
           .when('/', {
-            controller: 'HomeController as hC',
+            controller: 'HomeController as homeCtrl',
             templateUrl: '/public/app/views/home.html',
             resolve: {
               load: ['InjectFileService', function(injectFile){
@@ -13,8 +13,13 @@ angular
             },
           })
           .when('/events', {
-            controller: 'EventsController as eC',
+            controller: 'EventsController as eventsCtrl',
             templateUrl: '/public/app/views/events.html',
+            resolve: {
+              load: ['InjectFileService', function(injectFile){
+                return injectFile.set('css', '/public/css/home.css', 'homecss');
+              }],
+            },
           });
     });
 // TODO: handle 404 here
